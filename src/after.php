04296b74ebf8d4b980fe64d5df19c75609259c03
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-03-17 14:56:33 +0100

    Corrections

--- src/after.php
+++ src/after.php
@@ -1,50 +1,58 @@
 <?php
 
-// name = for who
-function pizzaOrder($pizzatype, $name) {
+function order_pizza($pizzatype, $customer) {
+    $price = calcul_price($pizzatype);
+    $address = get_address($customer);
+    $toPrint ='Creating new order... <br>';
+    $toPrint .= 'A ' . $pizzatype;  
+    $toPrint .= ' pizza_type should be sent to ' . $customer . '. <br>The address: ' . $address '<br>';
+    $toPrint .='The bill is €'.$price.'.<br>';
+    $toPrint .= "Order finished.<br><br>";
 
-    $type = $pizzatype;
-    $toPrint = 'A ';
-    $toPrint .= $pizzatype;
-    $price = calculatCost($type);
-    $address = 'unknown';
-    switch($name){
-        case 'nico';
-            $address = 'somewhere in Belgium';
-            break;
-        case 'students';
-            $address ='BeCode office';
-            break;
-    }
-        echo 'Creating new order... <br>';
-    $toPrint .=   ' pizza should be sent to ' . $name . ". <br>The address: {$address}.";
-        echo $toPrint; echo '<br>';
-        echo'The bill is €'.$price.'.<br>';
-        echo "Order finished.<br><br>";
+    return $toPrint;
 }
-function calculatCost($pizza){
-    $cost = 'unknown';
+// C'est mieux si on fait une fonction pour l'address
 
-    switch ($pizza){
-        case 'marguerita';
-            $cost = 5;
+function get_address($customer){
+    
+    switch($customer){
+        case 'Koen';
+            return 'a peniche in Liège';
+            
+        case 'nico';
+            return 'somewhere in Belgium';
+            
+        case 'students';
+            return 'BeCode office';
+            
+        default:
+            return "Address missing, call the customer !";
+    }
+    // comme on est dans une fonction, on n'a pas besoin de mettre break dans switch
+}
+// C'est mieux de faire return dans la fonction et après on fait echo quand on l'appelle la fonction
+function calcul_price($pizza_type){
+    $price = 0; // comme c'est price c'est un chiffre, on l'intialise à 0. C'est mieux que 'unknown'
+    switch ($pizza_type){
+        case 'margherita';
+            $price = 5;
             break;
         case 'golden';
-            $cost = 100;
+            $price = 100;
             break;
         case 'calzone';
-            $cost = 10;
+            $price = 10;
             break;
+        default;
     }
-    return $cost;
+    return $price;
 }
 
-function ordr_piz_all(){
-    pizzaOrder('calzone', 'nico');
-    pizzaOrder('marguerita', 'nick');
-    pizzaOrder('golden', 'students');
-}
-ordr_piz_all();
+echo order_pizza('calzone', 'nico');
+echo order_pizza('margherita', 'nick');
+echo order_pizza('golden', 'students');
+
+
 
 
 
